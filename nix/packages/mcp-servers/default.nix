0a5b601495e4ff6da0b2825e32742838b0b3126c
Roman Gonzalez <open-source@roman-gonzalez.info>
2025-10-08 21:35:16 -0700

    bump: mcp-servers package

--- nix/packages/mcp-servers/default.nix
+++ nix/packages/mcp-servers/default.nix
@@ -8,16 +8,18 @@
   fetchFromGitHub,
   buildNpmPackage,
   nodejs,
+  nodePackages,
   python311,
   symlinkJoin,
 }:
 
 let
+  version = "2025.9.25";
   masterSrc = fetchFromGitHub {
     owner = "modelcontextprotocol";
     repo = "servers";
-    rev = "master";
-    sha256 = "sha256-X4Vr7QzAnZzWbotF1RFPgBoilG6apJypXRowvbpy8Mw=";
+    rev = version;
+    sha256 = "sha256-ysTuSHFs7GABMuFXG+DcyonVXVs7m45j9sDPdHBS2wQ=";
   };
 
   gitServer =
@@ -95,15 +97,41 @@ let
       package = pythonSet.mcp-server-fetch;
     };
 
+  # todo: remove once/if shx is in pkgs.nodePackages
+  shx =
+    let
+      version = "7c2dd8ce765ffb6b42964c9d8541706829487c90";
+    in
+      buildNpmPackage {
+	pname = "shx";
+	inherit version;
+	src = fetchFromGitHub {
+	  owner = "shelljs";
+	  repo = "shx";
+	  rev = version;
+	  hash = "sha256-QGq6WqjozgU9AxosDyTe7wrl+sulccxIN9AohoS+Zc0=";
+	};
+	dontNpmBuild = true;
+	npmDepsHash = "sha256-R1fn1TH4FntPnMd40AUGYIPLSZX18sQ7fKzTU3zSEd0=";
+	meta = with lib; {
+	  description = "Portable Shell Commands for Node";
+	  homepage = "https://github.com/shelljs/shx";
+	  license = licenses.mit;
+	  maintainers = with maintainers; [ roman ];
+	};
+      };
+
   jsServers = buildNpmPackage {
     pname = "mcp-servers";
-    version = "master";
+    inherit version;
     src = "${masterSrc}";
-    npmDepsHash = "sha256-QJo4EojeHBHTuKdv9QObGd07/z/y39Uq3JIHYGn2mCo=";
-    patches = [
-      ./main-branch-node-packages-lock.patch
-    ];
+    npmDepsHash = "sha256-iRPILytyloL6qRMvy2fsDdqkewyqEfcuVspwUN5Lrqw=";
     PUPPETEER_SKIP_DOWNLOAD = 1;
+    nativeBuildInputs = [
+      nodePackages.npm
+      nodePackages.typescript
+      shx
+    ];
     buildInputs = [ nodejs ];
     installPhase = ''
       mkdir $out
@@ -116,7 +144,7 @@ let
       ln -s $out/node_modules/@modelcontextprotocol/server-memory/dist/index.js $out/bin/mcp-server-sequentialthinking
     '';
     meta = with lib; {
-      description = "Google Drive Model Context Protocol server";
+      description = "Model Context Protocol servers";
       homepage = "https://modelcontextprotocol.io";
       license = licenses.mit;
       maintainers = with maintainers; [ roman ];
